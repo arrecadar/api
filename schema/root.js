741d4abd9cdb8aadfd23a723c196ecef87fe1c6e
Wallace Oliveira <wallace.oliveira@pagar.me>
2018-10-07 15:38:25 -0300

    app: add custom types

--- schema/root.js
+++ schema/root.js
@@ -1,6 +1,17 @@
 import { gql } from 'apollo-server-koa'
+import {
+  GraphQLDateTime,
+  GraphQLEmail,
+  GraphQLLimitedString,
+  GraphQLPassword,
+} from 'graphql-custom-types'
 
 export const Query = gql`
+  scalar Email
+  scalar DateTime
+  scalar Password
+  scalar MinString
+
   type Query {
     _empty: String
   }
@@ -11,5 +22,12 @@ export const Query = gql`
 `
 
 export const Resolvers = {
+  Email: GraphQLEmail,
+  DateTime: GraphQLDateTime,
+  Password: new GraphQLPassword(6, null, null, {
+    alphaNumeric: true,
+    specialChars: true,
+  }),
+  MinString: new GraphQLLimitedString(3),
   Mutation: {},
 }
